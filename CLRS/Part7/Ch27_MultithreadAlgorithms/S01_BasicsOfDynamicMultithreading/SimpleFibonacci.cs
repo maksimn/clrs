de85578768a2b3c6c17d561618ea0caa0a3579f9
maksimn <maksim1989sovunion@yandex.ru>
2016-11-18 00:40:47 +0300

    Начало

---
+++ CLRS/Part7/Ch27_MultithreadAlgorithms/S01_BasicsOfDynamicMultithreading/SimpleFibonacci.cs
@@ -0,0 +1,13 @@
+﻿namespace Books.CLRS.Part7.Ch27_MultithreadAlgorithms.S01_BasicsOfDynamicMultithreading {
+    public static class SimpleFibonacci {
+        public static int Fib(int n) {
+            if (n <= 1) {
+                return n;
+            } else {
+                int x = Fib(n - 1);
+                int y = Fib(n - 2);
+                return x + y;
+            }
+        }
+    }
+}
---
+++ CLRS/Part7/Ch27_MultithreadAlgorithms/S01_BasicsOfDynamicMultithreading/Tests/SimpleFibonacciTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using static Books.CLRS.Part7.Ch27_MultithreadAlgorithms.S01_BasicsOfDynamicMultithreading.SimpleFibonacci;
+
+namespace Books.CLRS.Part7.Ch27_MultithreadAlgorithms.S01_BasicsOfDynamicMultithreading.Tests {
+    [TestFixture]
+    class SimpleFibonacciTests {
+        [Test]
+        public void FibTest() {
+            var result = Fib(4);
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
