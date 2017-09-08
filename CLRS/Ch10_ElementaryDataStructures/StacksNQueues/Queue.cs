f3a6e9d8ec019b01d75b2a5a85aa106d8e89a3b7
maksimn <maksim1989sovunion@yandex.ru>
2017-09-08 18:29:32 +0300

    Queue class

---
+++ CLRS/Ch10_ElementaryDataStructures/StacksNQueues/Queue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Books.CLRS.Ch10_ElementaryDataStructures.StacksNQueues {
+    public class Queue<T> {
+        private List<T> array = new List<T>();
+        private int head = 0; // индекс головного элемента
+        private int tail = 0; // индекс добавления нового элемента
+
+        public void Enqueue(T x) {
+            array.Add(x);
+            tail++;
+        }
+
+        public T Dequeue() {
+            var x = array[head];
+            array[head] = default(T);
+            head++;
+            return x;
+        }
+    }
+}
---
+++ CLRS/Ch10_ElementaryDataStructures/StacksNQueues/Tests/QueueTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Books.CLRS.Ch10_ElementaryDataStructures.StacksNQueues.Tests {
+    [TestFixture]
+    class QueueTests {
+        [Test]
+        public void Exercise_10_1_3() {
+            var queue = new Queue<int>();
+            queue.Enqueue(4);
+            queue.Enqueue(1);
+            queue.Enqueue(3);
+            var res1 = queue.Dequeue();
+            queue.Enqueue(8);
+            var res2 = queue.Dequeue();
+
+            Assert.AreEqual(4, res1);
+            Assert.AreEqual(1, res2);
+        }
+    }
+}
