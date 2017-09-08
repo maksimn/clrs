1cd7dbffc8a14692d1f9af56b9e55ac63aac61f3
maksimn <maksim1989sovunion@yandex.ru>
2017-09-09 01:52:58 +0300

    Linked list

---
+++ CLRS/Ch10_ElementaryDataStructures/LinkedLists/LinkedList.cs
@@ -0,0 +1,35 @@
+﻿namespace Books.CLRS.Ch10_ElementaryDataStructures.LinkedLists {
+    public class LinkedList<T> {
+        public ListNode Head { get; private set; }
+
+        public ListNode Search(int key) {
+            var x = Head;
+            
+            while (x != null && x.Key != key) {
+                x = x.Next;
+            }
+
+            return x;
+        }
+
+        public void Insert(ListNode node) {
+            node.Next = Head;
+            if(Head != null) {
+                Head.Prev = node;
+            }
+            Head = node;
+            node.Prev = null;
+        }
+
+        public void Delete(ListNode node) {
+            if (node.Prev != null) {
+                node.Prev.Next = node.Next;
+            } else {
+                Head = node.Next;
+            }
+            if (node.Next != null) {
+                node.Next.Prev = node.Prev;
+            }
+        }
+    }
+}
---
+++ CLRS/Ch10_ElementaryDataStructures/LinkedLists/ListNode.cs
@@ -0,0 +1,8 @@
+﻿namespace Books.CLRS.Ch10_ElementaryDataStructures.LinkedLists {
+    public class ListNode {
+        public int Key { get; set; }
+        public object Value { get; set; }
+        public ListNode Prev { get; set; }
+        public ListNode Next { get; set; }
+    }
+}
