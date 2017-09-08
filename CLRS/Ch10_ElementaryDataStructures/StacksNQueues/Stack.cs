5fe267e4a28eae1a00b41183c2267241d5b712a1
maksimn <maksim1989sovunion@yandex.ru>
2017-09-08 16:46:44 +0300

    Stack class

---
+++ CLRS/Ch10_ElementaryDataStructures/StacksNQueues/Stack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Books.CLRS.Ch10_ElementaryDataStructures.StacksNQueues {
+    public class Stack<T> {
+        private List<T> array = new List<T>();
+        private int top = -1;
+
+        public void Push(T x) {
+            if (top == array.Count - 1) {
+                array.Add(x); 
+                top++;
+            } else {
+                top++;
+                array[top] = x;
+            }
+        }
+
+        public T Pop() {
+            if (StackEmpty()) {
+                throw new InvalidOperationException("You cannot pop element from an empty stack.");
+            }
+
+            top--;
+            return array[top + 1];
+        }
+
+        public bool StackEmpty() {
+            return top == -1;
+        }
+    }
+}
---
+++ CLRS/Ch10_ElementaryDataStructures/StacksNQueues/Tests/StackTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Books.CLRS.Ch10_ElementaryDataStructures.StacksNQueues.Tests {
+    [TestFixture]
+    class StackTests {
+        [Test]
+        public void Exercise_10_1_1() {
+            var stack = new Stack<int>();
+            stack.Push(4);
+            stack.Push(1);
+            stack.Push(3);
+            var res1 = stack.Pop();
+            stack.Push(8);
+            var res2 = stack.Pop();
+
+            Assert.AreEqual(3, res1);
+            Assert.AreEqual(8, res2);
+        }
+    }
+}
