b450651bffb60af556ae87dfee84144c4baa7036
maksimn <maksim1989sovunion@yandex.ru>
2017-09-18 04:08:56 +0300

    Hash Table Chain Based

---
+++ CLRS/Ch11_HashingAndHashTables/HashTableChainBased.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Books.CLRS.Ch11_HashingAndHashTables {
+    public class HashTableChainBased<T> {
+        private LinkedList<T>[] hashTable;
+
+        private const int defaultSize = 100;
+        private int size;
+
+        public HashTableChainBased() {
+            InstantiateHashTable(defaultSize);
+        }
+
+        public HashTableChainBased(int numCells) {
+            InstantiateHashTable(numCells);
+        }
+
+        private void InstantiateHashTable(int size) {
+            hashTable = new LinkedList<T>[size];
+            for (var i = 0; i < hashTable.Length; i++) {
+                hashTable[i] = new LinkedList<T>();
+            }
+            this.size = size;
+        }
+
+        private int GetIndexForHashTable(T value) {
+            int index = value.GetHashCode() % size;
+            return index > -1 ? index : -index;
+        }
+
+        public void Insert(T value) {
+            int index = GetIndexForHashTable(value);
+            hashTable[index].AddLast(value);
+        }
+
+        public void Delete(T value) {
+            int index = GetIndexForHashTable(value);
+            hashTable[index].Remove(value);
+        }
+
+        public bool Search(T value) {
+            int index = GetIndexForHashTable(value);
+            var node = hashTable[index].Find(value);
+            return node != null;
+        }
+    }
+}
---
+++ CLRS/Ch11_HashingAndHashTables/Tests/HashTableChainBasedTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+namespace Books.CLRS.Ch11_HashingAndHashTables.Tests {
+    [TestFixture]
+    class HashTableChainBasedTests {
+        [Test]
+        public void HashTableTest1() {
+            var hashTable = new HashTableChainBased<string>(10);
+
+            hashTable.Insert("Amanda");
+            hashTable.Insert("Elizabeth");
+            hashTable.Insert("Anastasia");
+            hashTable.Insert("Helen");
+            hashTable.Insert("Elena");
+            hashTable.Insert("Yulia");
+            hashTable.Insert("Julia");
+            hashTable.Insert("Vera");
+            hashTable.Insert("Agniya");
+            hashTable.Insert("Regina");
+
+            Assert.AreEqual(false, hashTable.Search("Maxim"));
+            Assert.AreEqual(true, hashTable.Search("Elena"));
+        }
+    }
+}
